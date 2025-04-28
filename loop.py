77b19e99cb47c1f8349f4a9622561add02eb4680
anityam <89035703+anityam@users.noreply.github.com>
2025-04-27 19:24:44 -0500

    added loooping

--- loop.py
+++ loop.py
@@ -1,2 +1,49 @@
-print("This is loop file")
+### complex variable 
+nameList = ["Ram", "shyam", "sita"]
+
+
+print ( " My name is ", nameList)
+
+# for i in nameList:
+#     print("My name is "+ i)
+#     print("working in loop")
+
+
+print(nameList[0])
+
+## to add to a list
+nameList.append("bjn")
+print(nameList[3])
+
+
+
+## add to distinct place
+nameList.insert(1,"ram")
+print(nameList)
+
+
+## remove to a list 
+nameList.pop()
+print(nameList)
+
+
+### calculate 
+totalNumberOfRam = nameList.count("ram")
+print(totalNumberOfRam)
+
+## counting
+total = len(nameList)
+print("total item", total)
+
+### Control flow statement
+
+## if - elif - else
+
+for i in nameList:
+    if i.lower() == "sita":
+        print ("Yes there is sita")
+    elif i.lower() == "ram":
+        print ("Yes there is ram")
+    else:
+        print("")
 
---
+++ variables.py
@@ -0,0 +1,33 @@
+print("This is loop file")
+
+
+name = "Shristi"  # string are always in code
+wString = "•ªº"
+number = "123"
+city = "NewHIll"
+whole = 12
+whjahsdkhaklsdjklas = 12.23
+
+
+## what is the type of thing being used
+print(type(whole))
+print(type(whole/12))
+
+
+print(whjahsdkhaklsdjklas/12.23)
+
+### complex type of variables
+numberList = [1,2,3,4,5]
+mixedList = ["one",1,3,5]
+
+### dictionary
+numberDict = {1: "dallas", 2: "Houston"}
+
+print(numberList)
+print(mixedList)
+
+print("My name is " + name)  # can do this since both item are same kind i.e string
+print (wString + city)
+# print ("My name is " + number ) # cannot do this since different types
+print ("My favor number is ", number,"this is another one") 
+print(number+number)
